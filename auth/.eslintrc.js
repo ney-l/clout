ec33686e2c3357a68494b91b2f8ef762cd860c4f
Ney Lanjewar <neylanjewar@gmail.com>
2022-03-05 21:25:58 +0530

    configures prettier

--- auth/.eslintrc.js
+++ auth/.eslintrc.js
@@ -3,17 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-typescript-prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
+    '@typescript-eslint/no-console': [
+      'error',
+      { allow: ['warn', 'error', 'info'] },
+    ],
+    '@typescript-eslint/quotes': ['single'],
   },
 };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,1 +1,13 @@
-import express from "express";
+import express from 'express';
+
+const app = express();
+
+app.get('*', (req, res) => {
+  res.json({ message: 'hello' });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.info(`Server listening on ${PORT}`);
+});
